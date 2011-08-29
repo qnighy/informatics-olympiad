ca63f87f82f6d29c51d59ca392d496415ad12213
Masaki Hara <ackie.h.gmai@gmail.com>
2011-08-30 07:43:07 +0900

    modified tracking code

--- sptr.js
+++ sptr.js
@@ -19,14 +19,3 @@ head.js("jquery-1.5.1.min.js", function() {
     console.log($(this).getCaretPos());
   });
 }); */
-
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-5840991-4']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
